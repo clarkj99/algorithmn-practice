6458e36760696d166963c6c27109f1cea2b6659a
clarkj99 <clark@stontech.net>
2020-06-24 22:12:33 -0400

    reversedString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed, character) => character + reversed, '')
+}
 
 module.exports = reverse;
+
+
+
+// function reverse(str) {
+//     let reversedString = '';
+//     for (let character of str) {
+//         reversedString = character + reversedString
+//     }
+//     return reversedString
+// }
+
+
+// function reverse(str) {
+//     let reversedString = '';
+//     for (let i = str.length; i > 0; i--) {
+//         reversedString += str.substr(i - 1, 1)
+//     }
+//     return reversedString
+// }
