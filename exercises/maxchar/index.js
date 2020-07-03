3f61b2f4b703b7a7d7f40d0efa18945afd92b980
clarkj99 <clark@stontech.net>
2020-07-03 09:37:27 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+    const charCount = {}
+    let maxCount = 0;
+    let result = "";
+    for (let char of str.split('')) {
+        charCount[char] = charCount[char] + 1 || 1;
+        if (charCount[char] > maxCount) {
+            result = char;
+            maxCount = charCount[char];
+        }
+    }
+    return result;
+}
 module.exports = maxChar;
