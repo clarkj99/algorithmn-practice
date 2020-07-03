e62bae9c798cdcf6ebe17e5e2f0d4efb19f61e9d
clarkj99 <clark@stontech.net>
2020-07-02 23:13:33 -0400

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let nString = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+
+    return parseInt(nString) * Math.sign(n)
+}
 
 module.exports = reverseInt;
